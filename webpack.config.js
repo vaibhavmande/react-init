278a01e821f7b66c02be535ff61f3d29786df04f
vaibhavmande <vaibhavrmande@gmail.com>
2023-01-06 11:41:21 -0500

    add webpack and configurations

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+  },
+};
