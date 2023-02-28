b0d9475901e01bbb7226f75d7dbebc6c208044eb
vaibhavmande <vaibhavrmande@gmail.com>
2023-02-28 16:05:19 -0500

    clean login page

--- src/core/routes/AuthenticatedAppRoutes.tsx
+++ src/core/routes/AuthenticatedAppRoutes.tsx
@@ -11,6 +11,7 @@ function AuthenticatedAppRoutes() {
       <Route path="/" element={<HomePage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/logout" element={<p>Logout page</p>} />
       <Route path="*" element={<p>Page not found</p>} />
     </Routes>
   );
